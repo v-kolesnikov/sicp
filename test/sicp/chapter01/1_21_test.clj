bc2cd1179701d5609a55166fd3e7f172186dab59
kolesnikov <re.vkolesnikov@gmail.com>
2015-09-09 15:18:28 +0300

    Add a solution of the exercise 1-21

---
+++ test/sicp/chapter01/1_21_test.clj
@@ -0,0 +1,9 @@
+(ns sicp.chapter01.1-21-test
+  (:require [clojure.test :refer :all]
+            [sicp.test-helper :refer :all]
+            [sicp.chapter01.1-21 :refer :all]))
+
+(deftest test-gcd
+  (assert-equal 199 (smallest-divisor 199))
+  (assert-equal 1999 (smallest-divisor 1999))
+  (assert-equal 7 (smallest-divisor 19999)))
