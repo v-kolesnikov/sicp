c0ce8bb600a0c02df74fa9bdcd4b4a934d87330f
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-10-13 01:30:41 +0300

    Fix exercise 1-20

--- src/sicp/chapter01/1_20.clj
+++ src/sicp/chapter01/1_20.clj
@@ -2,14 +2,8 @@
   (:require [sicp.common :refer [square]]))
 
 (defn gcd
-  ; Return hash with GCD and iterations count
   [a b]
-  (loop [a a
-         b b
-         n 0]
-    (cond
-      (zero? b) {:nod a :iter n}
-      :else (recur b
-                   (rem a
-                        b)
-                   (inc n)))))
+  (loop [a (Math/abs a)
+         b (Math/abs b)]
+    (if (zero? b) a,
+        (recur b (mod a b)))))
