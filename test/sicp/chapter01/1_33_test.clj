27dbb570c3e80a2d604c9da843008410293fa42f
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-11-07 16:42:03 +0300

    Fix exercise 1-33 tests

--- test/sicp/chapter01/1_33_test.clj
+++ test/sicp/chapter01/1_33_test.clj
@@ -3,6 +3,10 @@
             [sicp.test-helper :refer :all]
             [sicp.chapter01.1-33 :refer :all]))
 
-(deftest test-filtered-accumulate
+(deftest test-summ-of-primes-squares
   (assert-equal 88 (summ-of-primes-squares 1 10))
-  (assert-equal 189 (product-of-pos-coprimes 1 10)))
+  (assert-equal 65797 (summ-of-primes-squares 1 100)))
+
+(deftest test-product-of-pos-coprimes
+  (assert-equal 189 (product-of-pos-coprimes 1 10))
+  (assert-equal 8729721 (product-of-pos-coprimes 1 20)))
