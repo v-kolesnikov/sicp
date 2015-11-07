f2a3df5c97c97225a03008f3aadd1f1700adc538
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-11-07 20:48:39 +0300

    Refactoring common.clj

--- src/sicp/common.clj
+++ src/sicp/common.clj
@@ -1,11 +1,9 @@
 (ns sicp.common)
 
-; since 1.1.4
 (defn square [x]
   (* x x))
 
 (defn avg
-  ; Return the average value of the collection
-  [coll]
-  (float (/ (reduce + coll)
-            (count coll))))
+  [seq]
+  (float (/ (reduce + seq)
+            (count seq))))
