e1da76fcd685012e5a73e65d20503eac2b7801f3
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-11-17 20:09:19 +0300

    Update project dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,11 @@
   :plugins [[lein-kibit "0.1.2"]
             [lein-cljfmt "0.3.0"]
             [lein-cloverage "1.0.6"]
-            [jonase/eastwood "0.2.1"]]
+            [jonase/eastwood "0.2.2"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.trace "0.7.8"]]
+                 [org.clojure/tools.trace "0.7.8"]
+                 [cljfmt "0.3.0"]
+                 [jonase/eastwood "0.2.2"]]
   :main ^:skip-aot sicp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
