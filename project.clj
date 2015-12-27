efb455520b555877b6f1bd1a4fd86aa9f5d3ab77
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-12-27 18:46:27 +0300

    Add clojure/test.check 0.9.0 plugin

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :plugins [[lein-kibit "0.1.2"]
             [lein-cljfmt "0.3.0"]
             [jonase/eastwood "0.2.2"]]
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/test.check "0.9.0"]]
   :main ^:skip-aot sicp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
