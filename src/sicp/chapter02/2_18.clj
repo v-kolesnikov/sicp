9b264ddaf5a6dfad01d6ce06cbf57ebfb5c1d2b8
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-01-04 13:34:07 +0300

    Add a solution of the exercise 2-18

---
+++ src/sicp/chapter02/2_18.clj
@@ -0,0 +1,10 @@
+(ns sicp.chapter02.2-18)
+
+(defn reverse
+  [coll]
+  (loop [s coll
+         acc (list)]
+    (if (empty? s)
+      acc
+      (recur (rest s)
+             (conj acc (first s))))))
