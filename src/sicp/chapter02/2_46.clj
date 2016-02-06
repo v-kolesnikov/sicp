8214910c710aef16ec06b72e89b7a2b5c93d76b4
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-02-06 22:40:00 +0300

    Add a solution of the exercise 2-46

---
+++ src/sicp/chapter02/2_46.clj
@@ -0,0 +1,30 @@
+(ns sicp.chapter02.2-46)
+
+(defn make-vect
+  [x y]
+  {:pre [(number? x) (number? y)]}
+  (vector x y))
+
+(defn xcor-vect
+  [[x _]]
+  x)
+
+(defn ycor-vect
+  [[_ y]]
+  y)
+
+(defn add-vect
+  [[x1 y1] [x2 y2]]
+  (make-vect (+ x1 x2)
+             (+ y1 y2)))
+
+(defn sub-vect
+  [[x1 y1] [x2 y2]]
+  (make-vect (- x1 x2)
+             (- y1 y2)))
+
+(defn scale-vect
+  [[x y] s]
+  {:pre [(number? s)]}
+  (make-vect (* x s)
+             (* y s)))
