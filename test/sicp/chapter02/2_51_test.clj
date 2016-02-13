86be9225563954516c80740fe34f9f7fc35efc03
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-02-13 13:07:57 +0300

    Fix solution of the exercise 2-51

--- test/sicp/chapter02/2_51_test.clj
+++ test/sicp/chapter02/2_51_test.clj
@@ -4,20 +4,34 @@
             [sicp.chapter02.2-51 :as sicp-2-51]
             [sicp.test-helper :refer [assert-equal]]))
 
-(deftest test-below
-  (let [painter (sicp-2-51/below identity identity)]
-    (assert-equal [(make-frame [0.0 0.0] [1.0 0.0] [0.0 0.5])
-                   (make-frame [0.0 0.5] [1.0 0.0] [0.0 0.5])]
-                  (painter (make-frame [0.0 0.0] [1.0 0.0] [0.0 1.0])))
-    (assert-equal [(make-frame [2.0 2.0] [1.0 0.0] [0.0 0.5])
-                   (make-frame [2.0 2.5] [1.0 0.0] [0.0 0.5])]
-                  (painter (make-frame [2.0 2.0] [1.0 0.0] [0.0 1.0])))))
+(def frame? vector?)
 
-(deftest test-below-rotate
-  (let [painter (sicp-2-51/below-rotate identity identity)]
-    (assert-equal [(make-frame [0.0 0.0] [1.0 0.0] [0.0 0.5])
-                   (make-frame [0.0 0.5] [1.0 0.0] [0.0 0.5])]
-                  (painter (make-frame [0.0 0.0] [1.0 0.0] [0.0 1.0])))
-    (assert-equal [(make-frame [2.0 2.0] [1.0 0.0] [0.0 0.5])
-                   (make-frame [2.0 2.5] [1.0 0.0] [0.0 0.5])]
-                  (painter (make-frame [2.0 2.0] [1.0 0.0] [0.0 1.0])))))
+(defn draw [picture]
+  (fn [frame]
+    (swap! picture conj frame)))
+
+(deftest test-below
+  (let [picture (atom [] :validator frame?)
+        painter (sicp-2-51/below (draw picture) (draw picture))]
+    (assert-equal ['([0.0 0.0] [1.0 0.0] [0.0 0.5])
+                   '([0.0 0.5] [1.0 0.0] [0.0 0.5])]
+                  (do (painter (make-frame [0.0 0.0] [1.0 0.0] [0.0 1.0])) @picture)))
+
+  (let [picture (atom [] :validator frame?)
+        painter (sicp-2-51/below (draw picture) (draw picture))]
+    (assert-equal ['([5.0 5.0] [2.0 1.0] [0.5 1.0])
+                   '([5.5 6.0] [2.0 1.0] [0.5 1.0])]
+                  (do (painter (make-frame [5.0 5.0] [2.0 1.0] [1.0 2.0])) @picture))))
+
+(deftest test-beside
+  (let [picture (atom [] :validator frame?)
+        painter (sicp-2-51/beside (draw picture) (draw picture))]
+    (assert-equal ['([0.0 0.0] [0.5 0.0] [0.0 1.0])
+                   '([0.5 0.0] [0.5 0.0] [0.0 1.0])]
+                  (do (painter (make-frame [0.0 0.0] [1.0 0.0] [0.0 1.0])) @picture)))
+
+  (let [picture (atom [] :validator frame?)
+        painter (sicp-2-51/below (draw picture) (draw picture))]
+    (assert-equal ['([5.0 5.0] [2.0 1.0] [0.5 1.0])
+                   '([5.5 6.0] [2.0 1.0] [0.5 1.0])]
+                  (do (painter (make-frame [5.0 5.0] [2.0 1.0] [1.0 2.0])) @picture))))
