cc4f3956620798e56c5c03be3b5271a0b1822522
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-08-07 14:42:49 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :plugins [[lein-kibit "0.1.2"]
-            [lein-cljfmt "0.4.1"]
+            [lein-cljfmt "0.5.3"]
             [lein-gen "0.2.1"]
-            [jonase/eastwood "0.2.2"]]
+            [jonase/eastwood "0.2.3"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/test.check "0.9.0"]
-                 [rhizome "0.2.5"]
+                 [rhizome "0.2.7"]
                  [quil "2.4.0"]]
   :generators [[sicp-generator "0.1.2"]]
   :main ^:skip-aot sicp.core
