5a1eb660735ed8c9b15c8163868f83c49297f820
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-10-04 19:48:01 +0300

    Update project.clj
     * Refactoring profiles

--- project.clj
+++ project.clj
@@ -3,15 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :plugins [[lein-kibit "0.1.2"]
-            [lein-cljfmt "0.5.3"]
-            [lein-gen "0.2.1"]
-            [jonase/eastwood "0.2.3"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/test.check "0.9.0"]
                  [rhizome "0.2.7"]
                  [quil "2.4.0"]]
-  :generators [[sicp-generator "0.1.2"]]
   :main ^:skip-aot sicp.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev [:project/dev :profiles/dev]
+             :project/dev {:dependencies [[slamhound "1.5.5"]]
+                           :generators [[sicp-generator "0.1.2"]]
+                           :plugins [[lein-kibit "0.1.2"]
+                                     [lein-cljfmt "0.5.3"]
+                                     [lein-gen "0.2.1"]
+                                     [jonase/eastwood "0.2.3"]]
+                           :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
+             :profiles/dev {}
+             :profiles/test {}})
